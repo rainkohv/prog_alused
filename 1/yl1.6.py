b57527bcd2a782aadd13b3f33368714e0bd46d2e
Rain Kohv <rain.kohv@khk.ee>
2020-09-22 10:41:38 +0300

    Koostatud ülesanne 1.6 lahendus

---
+++ 1/yl1.6.py
@@ -0,0 +1,8 @@
+
+max_kohad_bussis = int (input("Sisestage kohtade arv bussis: "))
+inimeste_arv = int(input("Sisestage inimeste arv: "))
+busside_arv = int(inimeste_arv / max_kohad_bussis)
+maha_inimesed =  inimeste_arv % max_kohad_bussis
+
+print("Inimeste transpordiks on vajalik tellida " + str(busside_arv) + " buss(i).")
+print("Bussi(desse) ei mahu " + str(maha_inimesed) + " inimest.")
