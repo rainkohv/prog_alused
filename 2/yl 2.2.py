4d84cf394bdf6cc7b0857c78b8f0880483eab205
Rain Kohv <rain.kohv@khk.ee>
2020-09-22 14:26:20 +0300

    Koostatud ülesanne 2.2 lahendus (optimeeritud)

--- 2/yl 2.2.py
+++ 2/yl 2.2.py
@@ -1,13 +1,14 @@
+#nime küsimine
 name = input("Sisestage leedu perekonnanimi: ")
-
+#kui nime lõpus -ne, prindi "abielus"
 if name[-2:] == "ne":
     print("Abielus")
-
+#kui nime lõpus -ne, prindi "vallaline"
 elif name[-2:] == "te":
     print("Vallaline")
-
-elif name[-1:] == "e" and name[-2:] != "te" and name[-2:] != "ne":
+#kui nime lõpus -e, prindi "määramata"
+elif name[-1:] == "e":
     print("Määramata")
-
+#kui ei vasta ühelegi tingimusele, prindi "pole ilmselt leedu perekonnanimi"
 else:
     print("Pole ilmselt leedu perekonnanimi")
