89043f736cac6e0f89ab46ac95c9ba6e2c470b66
Rain Kohv <rain.kohv@khk.ee>
2020-09-22 14:19:06 +0300

    Koostatud ülesanne 2.2 lahendus

--- 2/yl 2.2.py
+++ 2/yl 2.2.py
@@ -3,11 +3,11 @@ name = input("Sisestage leedu perekonnanimi: ")
 if name[-2:] == "ne":
     print("Abielus")
 
-if name[-2:] == "te":
+elif name[-2:] == "te":
     print("Vallaline")
 
-if name[-1:] == "e" and name[-2:] != "te" and name[-2:] != "ne":
+elif name[-1:] == "e" and name[-2:] != "te" and name[-2:] != "ne":
     print("Määramata")
 
-else :
+else:
     print("Pole ilmselt leedu perekonnanimi")
